14ae114a90975a4c8746bf48ee66fffde726894c
esastrelois <estefania.sastre-lois.external@capgemini.com>
2024-06-18 15:58:18 +0200

    Evento Saluda añadido

--- demo/src/main/java/com/proyectoUno/ioc/MyConfig.java
+++ demo/src/main/java/com/proyectoUno/ioc/MyConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.event.EventListener;
 
 @Configuration
 public class MyConfig {
@@ -13,12 +14,17 @@ public class MyConfig {
 	@Bean
 	int contInit() { return contadorInit; }
 	
-	
 	@Bean
 	@Scope("prototype")
 //	Entorno entorno(@Value("${app.contador.init:1}") int contInit) {
 	Entorno entorno(int contInit) {
 		return new EntornoImpl(contInit);
+	}
+	
+	//EventListener para el evento creado en SaludaImpl
+	@EventListener
+	void trataEvento(SaludaImpl.saludaEvent ev) {
+		System.err.println("Evento -> "+ev.tipo() + " -> " + ev.destinatario());
 	}
 	
 }
--- demo/src/main/java/com/proyectoUno/ioc/SaludaImpl.java
+++ demo/src/main/java/com/proyectoUno/ioc/SaludaImpl.java
@@ -2,6 +2,7 @@ package com.proyectoUno.ioc;
 
 import java.util.Optional;
 
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.lang.NonNull;
@@ -12,20 +13,49 @@ import org.springframework.stereotype.Component;
 @Scope("prototype")
 @Profile("es")
 public class SaludaImpl implements Saluda {
-Entorno entorno;
+	//Clase estática para el evento
+	public static class SaludaEvento {
+		private String tipo;
+		private String destinatario;
+		
+		public SaludaEvento(String tipo, String destinatario) {
+			this.tipo = tipo;
+			this.destinatario = destinatario;
+		}
+		public String tipo() {
+			return tipo;
+		}
+		public String destinatario() {
+			return destinatario;
+		}
+	}
 	
-	public SaludaImpl(Entorno entorno) {
+	//Record para el evento
+	public static record saludaEvent(String tipo, String destinatario) { }
+	
+	private ApplicationEventPublisher publisher;
+	private Entorno entorno;
+	
+	public SaludaImpl(Entorno entorno, ApplicationEventPublisher publisher) {
 		this.entorno = entorno;
+		this.publisher = publisher;
 	}
 
+	/* Publica el evento */
+	protected void onSaluda(@NonNull String tipo, @NonNull String destinatario) {
+		publisher.publishEvent(new saludaEvent(tipo, destinatario));
+	}
+	
 	@Override
 	public void saluda(@NonNull String nombre) {
-		if(nombre == null)
-			throw new IllegalArgumentException("El nombre es obligatorio.");
+//		if(nombre == null)
+//			throw new IllegalArgumentException("El nombre es obligatorio.");
 		entorno.write("Hola " + nombre.toUpperCase());
+		onSaluda("saluda",nombre);
 	}
 	public void saluda() {
 		entorno.write("Hola");
+		onSaluda("saluda ","sin nombre");
 	}
 
 	@Override
