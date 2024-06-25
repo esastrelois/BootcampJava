abb6672557aa0451e83adcf0f356fca94865b167
esastrelois <estefania.sastre-lois.external@capgemini.com>
2024-06-25 13:56:06 +0200

    Adicción de nucleo core y excepciones - de uso general

---
+++ catalogo/src/main/java/com/catalogo/domains/core/contracts/services/DomainService.java
@@ -0,0 +1,23 @@
+package com.catalogo.domains.core.contracts.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+
+import com.catalogo.exceptions.InvalidDataException;
+
+//Contiene las operaciones CRUD generales
+public interface DomainService<E, K> {
+	List<E> getAll();
+	
+	Optional<E> getOne(K id);
+	
+	E add(E item) throws DuplicateKeyException, InvalidDataException;
+	
+	E modify(E item) throws NotFoundException, InvalidDataException;
+	
+	void delete(E item) throws InvalidDataException;
+	void deleteById(K id);
+}
