c5fa2cd84ccbf472c4b26bd06530a296b38e2eb7
esastrelois <estefania.sastre-lois.external@capgemini.com>
2024-06-27 16:08:32 +0200

    Procesador de datos añadido a lotes-xml

---
+++ lotes-xml/src/main/java/com/example/models/Persona.java
@@ -0,0 +1,18 @@
+package com.example.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+/* 
+ * Objeto con los campos que tiene la tabla en la base de datos 
+ */
+public class Persona {
+	private long id;
+	private String nombre;
+	private String correo;
+	private String ip;
+}
