d5c16b42e6502ebbd4994178657ffa15cbc669a6
esastrelois <estefania.sastre-lois.external@capgemini.com>
2024-06-27 13:00:03 +0200

    PersonasCsvItemReader añadido (Lector de ficheros)

---
+++ lotes-java/src/main/java/com/example/batch/PersonasBatchConfiguration.java
@@ -0,0 +1,44 @@
+package com.example.batch;
+
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.example.models.Persona;
+import com.example.models.PersonaDTO;
+
+@Configuration
+public class PersonasBatchConfiguration {
+	@Autowired
+	JobRepository jobRepository;
+	
+	@Autowired
+	PlatformTransactionManager transactionManager;
+
+	/*
+	 * Lector de ficheros
+	 */
+	public FlatFileItemReader<PersonaDTO> personaCSVItemReader(String fname) {
+		return new FlatFileItemReaderBuilder<PersonaDTO>().name("personaCSVItemReader")
+				.resource(new FileSystemResource(fname))
+				.linesToSkip(1)
+				.delimited()
+				.names(new String[] { "id", "nombre", "apellidos", "correo", "sexo", "ip" })
+				.fieldSetMapper(new BeanWrapperFieldSetMapper<PersonaDTO>() { { //BeanWrapperFieldSetMapper es el que divide la linea del csv a los campos que le hemos pasado
+						setTargetType(PersonaDTO.class);
+					}})
+				.build();
+	}
+	
+}
