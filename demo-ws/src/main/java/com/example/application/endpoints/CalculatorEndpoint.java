112433618e0f3e405dc34b1104a189b2b8bcdd50
esastrelois <estefania.sastre-lois.external@capgemini.com>
2024-07-01 09:01:50 +0200

    Division mejorada y WebServiceConfig añadido

--- demo-ws/src/main/java/com/example/application/endpoints/CalculatorEndpoint.java
+++ demo-ws/src/main/java/com/example/application/endpoints/CalculatorEndpoint.java
@@ -1,9 +1,13 @@
 package com.example.application.endpoints;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.ws.soap.SoapFaultException;
 
 import com.example.webservices.schemas.calculator.AddRequest;
 import com.example.webservices.schemas.calculator.AddResponse;
@@ -45,9 +49,16 @@ public class CalculatorEndpoint {
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "divRequest")
 	@ResponsePayload
 	public DivResponse div(@RequestPayload DivRequest request) {
+		if(request.getOp2() == 0)
+//			throw new ArithmeticException("/ by zero");
+			throw new SoapFaultException("Division by 0", new ArithmeticException("/ by zero")); //IllegalArgumentException();
 		var result = new DivResponse();
-		result.setDivResult(request.getOp1() / request.getOp2());
+		result.setDivResult(redondea(request.getOp1() / request.getOp2()));
 		return result;
+	}
+	
+	private double redondea(double o) {
+		return (new BigDecimal(o)).setScale(16, RoundingMode.HALF_UP).doubleValue();
 	}
 	
 }
