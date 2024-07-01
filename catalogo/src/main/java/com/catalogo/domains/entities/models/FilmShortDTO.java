7911573f80b5fa4de664d19f651bf6da5067be7d
esastrelois <estefania.sastre-lois.external@capgemini.com>
2024-07-01 08:41:10 +0200

    Añadidas novedades al catalogo

---
+++ catalogo/src/main/java/com/catalogo/domains/entities/models/FilmShortDTO.java
@@ -0,0 +1,5 @@
+package com.catalogo.domains.entities.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record FilmShortDTO(@JsonProperty("id") int filmId, @JsonProperty("titulo") String title) { }
