4d008fb8850c04a61e543f9dcf64e1c69ea9dfa4
esastrelois <estefania.sastre-lois.external@capgemini.com>
2024-07-04 14:59:32 +0200

    Actualizadas propiedades para eureka server

--- demo/src/main/java/com/proyectoUno/DemoApplication.java
+++ demo/src/main/java/com/proyectoUno/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import com.proyectoUno.domains.contracts.proxies.CalculatorProxy;
@@ -19,6 +21,8 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
+@EnableDiscoveryClient
+@EnableFeignClients("com.proyectoUno.application.proxies")
 @OpenAPIDefinition(
         info = @Info(
                 title = "Microservicio: Catalogo de peliculas",
