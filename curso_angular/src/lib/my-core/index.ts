213739f1c60460e290c20682d316ea324bf990b0
esastrelois <estefania.sastre-lois.external@capgemini.com>
2024-07-10 13:08:41 +0200

    LoggerService añadido

--- curso_angular/src/lib/my-core/index.ts
+++ curso_angular/src/lib/my-core/index.ts
@@ -1,2 +1,2 @@
 export { MyCoreModule } from './my-core.module' //Exporta solo el MyCoreModule
-//Se podría exportar todo con: export * from './my-core.module'
+export * from './services/logger.service' //Con * exporta todo 
