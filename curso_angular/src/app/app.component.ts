5787b7cf13c1a64c36495ea5c104546e411b004a
esastrelois <estefania.sastre-lois.external@capgemini.com>
2024-07-12 08:22:16 +0200

    Correcciones

--- curso_angular/src/app/app.component.ts
+++ curso_angular/src/app/app.component.ts
@@ -4,16 +4,16 @@ import { SecurityModule } from './security';
 import { LoggerService, MyCoreModule } from '@my/core';
 import { NotificationComponent, NotificationModalComponent } from './main';
 import { DemosComponent } from './demos/demos.component';
+import { HomeComponent } from "./main/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SecurityModule, MyCoreModule, NotificationComponent,NotificationModalComponent, DemosComponent,],
+  imports: [RouterOutlet, SecurityModule, MyCoreModule, NotificationComponent, NotificationModalComponent, DemosComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title: string = 'curso_angular';
   constructor(log: LoggerService) {
     log.error('Es un error')
     log.warn('Es un warn')
